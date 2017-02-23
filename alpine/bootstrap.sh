77d090429b437a74217c2245650126b0c34d832b
Trond Mjøen <trond@twined.net>
2017-02-23 15:04:55 +0100

    Bind dev

--- alpine/bootstrap.sh
+++ alpine/bootstrap.sh
@@ -19,11 +19,9 @@ auto eth0
 iface eth0 inet dhcp
   hostname $HOST
 "
-BOOT_FS=${ROOT_FS:-ext4}
-ROOT_FS=${ROOT_FS:-ext4}
 
-FEATURES="ata ide scsi virtio base $ROOT_FS"
-MODULES="$ROOT_FS"
+FEATURES="ata ide scsi virtio base ext4"
+MODULES="ext4"
 
 REL=${REL:-3.5}
 MIRROR=${MIRROR:-http://nl.alpinelinux.org/alpine}
@@ -38,8 +36,8 @@ ARCH=$(uname -m)
 
 umount $BOOT_DEV || /bin/true
 umount $ROOT_DEV || /bin/true
-mkfs.$BOOT_FS -L boot $BOOT_DEV
-mkfs.$ROOT_FS -L root $ROOT_DEV
+mkfs.ext4 -L boot $BOOT_DEV
+mkfs.ext4 -L root $ROOT_DEV
 mkswap $SWAP_DEV
 
 mount $ROOT_DEV $ROOT
@@ -51,8 +49,8 @@ curl -s $MIRROR/v$REL/main/$ARCH/apk-tools-static-${APKV}.apk | tar xz
   --root $ROOT --initdb add alpine-base
 
 cat <<EOF > $ROOT/etc/fstab
-$ROOT_DEV  /       $ROOT_FS    defaults,noatime    0   0
-$BOOT_DEV  /boot   $BOOT_FS    defaults,noatime    0   1
+$ROOT_DEV  /       ext4    defaults,noatime    0   0
+$BOOT_DEV  /boot   ext4    defaults,noatime    0   1
 $SWAP_DEV  swap    swap        defaults            0   0
 EOF
 echo $REPO > $ROOT/etc/apk/repositories
@@ -77,7 +75,7 @@ cp /etc/resolv.conf $ROOT/etc
 echo ttyS0 >> $ROOT/etc/securetty
 
 mount --bind /proc $ROOT/proc
-# mount --bind /dev $ROOT/dev
+mount --bind /dev $ROOT/dev
 
 chroot $ROOT /bin/sh<<CHROOT
 apk update --quiet
@@ -85,17 +83,17 @@ apk update --quiet
 setup-hostname -n $HOST
 printf "$INTERFACES" | setup-interfaces -i
 
-rc-update -q add networking boot
-rc-update -q add urandom boot
-rc-update -q add crond
+rc-update add networking boot
+rc-update add urandom boot
+rc-update -add crond
 
-apk add --quiet openssh
+apk add openssh
 rc-update -q add sshd default
 
 mkdir /etc/mkinitfs
 echo features=\""$FEATURES"\" > /etc/mkinitfs/mkinitfs.conf
 
-apk add --quiet linux-grsec
+apk add linux-grsec
 
 CHROOT
 
