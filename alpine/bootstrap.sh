5f88ef6a9239552a6696845da7fd79b17c6942b7
Trond Mjøen <trond@twined.net>
2017-02-23 15:42:17 +0100

    Fix ver

--- alpine/bootstrap.sh
+++ alpine/bootstrap.sh
@@ -9,7 +9,7 @@ iface eth0 inet dhcp
 "
 
 ALPINE_VER=${ALPINE_VER:-"latest-stable"}
-APK_TOOLS_VER=${APK_TOOLS_VER:-"2.6.7-r0"}
+APK_TOOLS_VER=${APK_TOOLS_VER:-"2.6.8-r2"}
 ARCH=$(uname -m)
 MIRROR="http://nl.alpinelinux.org/alpine"
 
