4097f331bd9e3e2d398594221499cb52644b103e
Trond Mjøen <trond@twined.net>
2017-02-23 15:17:50 +0100

    Simplify

--- alpine/bootstrap.sh
+++ alpine/bootstrap.sh
@@ -27,39 +27,35 @@ REL=${REL:-3.5}
 MIRROR=${MIRROR:-http://nl.alpinelinux.org/alpine}
 REPO=$MIRROR/v$REL/main
 APKV=${APKV:-2.6.8-r2}
-BOOT_DEV=${BOOT_DEV:-/dev/sda}
-ROOT_DEV=${ROOT_DEV:-/dev/sdb}
-SWAP_DEV=${SWAP_DEV:-/dev/sdc}
 
-ROOT=${ROOT:-/mnt}
 ARCH=$(uname -m)
 
-umount $BOOT_DEV || /bin/true
-umount $ROOT_DEV || /bin/true
-mkfs.ext4 -L boot $BOOT_DEV
-mkfs.ext4 -L root $ROOT_DEV
-mkswap $SWAP_DEV
+umount /dev/sda || /bin/true
+umount /dev/sdb || /bin/true
+mkfs.ext4 -L boot /dev/sda
+mkfs.ext4 -L root /dev/sdb
+mkswap /dev/sdc
 
-mount $ROOT_DEV $ROOT
-mkdir $ROOT/boot
-mount $BOOT_DEV $ROOT/boot
+mount /dev/sdb /mnt
+mkdir /mnt/boot
+mount /dev/sda /mnt/boot
 
 curl -s $MIRROR/v$REL/main/$ARCH/apk-tools-static-${APKV}.apk | tar xz
 ./sbin/apk.static --repository $REPO --update-cache --allow-untrusted \
-  --root $ROOT --initdb add alpine-base
+  --root /mnt --initdb add alpine-base
 
-cat <<EOF > $ROOT/etc/fstab
-$ROOT_DEV  /       ext4    defaults,noatime    0   0
-$BOOT_DEV  /boot   ext4    defaults,noatime    0   1
-$SWAP_DEV  swap    swap        defaults            0   0
+cat <<EOF > /mnt/etc/fstab
+/dev/sdb  /       ext4        defaults,noatime    0   0
+/dev/sda  /boot   ext4        defaults,noatime    0   1
+/dev/sdc  swap    swap        defaults            0   0
 EOF
-echo $REPO > $ROOT/etc/apk/repositories
+echo $REPO > /mnt/etc/apk/repositories
 
-sed -i '/^tty[0-9]:/d' $ROOT/etc/inittab
-echo 'ttyS0::respawn:/sbin/getty -L ttyS0 115200 vt100' >> $ROOT/etc/inittab
+sed -i '/^tty[0-9]:/d' /mnt/etc/inittab
+echo 'ttyS0::respawn:/sbin/getty -L ttyS0 115200 vt100' >> /mnt/etc/inittab
 
-mkdir -p $ROOT/boot/grub
-cat <<EOF > $ROOT/boot/grub/grub.cfg
+mkdir -p /mnt/boot/grub
+cat <<EOF > /mnt/boot/grub/grub.cfg
 set root=(hd0)
 set default="Alpine Linux"
 set timeout=0
@@ -70,14 +66,14 @@ menuentry "Alpine Linux" {
 }
 EOF
 
-cp /etc/resolv.conf $ROOT/etc
+cp /etc/resolv.conf /mnt/etc
 
-echo ttyS0 >> $ROOT/etc/securetty
+echo ttyS0 >> /mnt/etc/securetty
 
-mount --bind /proc $ROOT/proc
-mount --bind /dev $ROOT/dev
+mount --bind /proc /mnt/proc
+mount --bind /dev /mnt/dev
 
-chroot $ROOT /bin/sh<<CHROOT
+chroot /mnt /bin/sh<<CHROOT
 apk update --quiet
 
 setup-hostname -n $HOST
@@ -88,7 +84,7 @@ rc-update add urandom boot
 rc-update add crond
 
 apk add openssh
-rc-update -q add sshd default
+rc-update add sshd default
 
 mkdir /etc/mkinitfs
 echo features=\""$FEATURES"\" > /etc/mkinitfs/mkinitfs.conf
@@ -97,9 +93,10 @@ apk add linux-grsec
 
 CHROOT
 
-umount $ROOT/proc
-umount $ROOT/boot
-umount $ROOT
+umount /mnt/proc
+umount /mnt/dev
+umount /mnt/boot
+umount /mnt
 
 echo "== Bootstrap finished. Type reboot to reboot system"
 
